91c410dcbbf017a13543a938357dbe3fbabf0007
vincentbons <vincent.bons@qbuzz.nl>
2021-04-19 13:04:35 +0200

    Oops. Used key instead of target key.

--- transpose.py
+++ transpose.py
@@ -34,12 +34,12 @@ def get_number_of_steps_for_transposition_to(midi_path, target_key):
     if mode == 'minor':
         target_key_nr -= 3
 
-    above_key_target_nr = key_nr
-    if above_key_target_nr < key_nr:
+    above_key_target_nr = target_key_nr
+    if above_key_target_nr < target_key_nr:
         above_key_target_nr += 12
 
-    below_key_target_nr = key_nr
-    if below_key_target_nr > key_nr:
+    below_key_target_nr = target_key_nr
+    if below_key_target_nr > target_key_nr:
         below_key_target_nr -= 12
 
     transpose_steps_down = key_nr - below_key_target_nr
